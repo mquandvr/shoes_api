e91821aec468d9236f9c9be117cf8d51fca57800
do.nguyen <codovan.nguyen@gmail.com>
2018-08-04 12:48:39 +0700

    implement Store model

---
+++ src/main/java/com/shoes/api/controller/StoreController.java
@@ -0,0 +1,32 @@
+package com.shoes.api.controller;
+
+import com.shoes.api.dto.StoreDTO;
+import com.shoes.api.service.StoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StoreController {
+
+    @Autowired
+    private StoreService storeService;
+
+    @RequestMapping(value = "/stores", method = RequestMethod.GET)
+    public List<StoreDTO> listCategory() {
+        return storeService.findAll();
+    }
+
+    @RequestMapping(value = "/stores/store", method = RequestMethod.POST)
+    public StoreDTO create(@RequestBody StoreDTO dto) {
+        return storeService.save(dto);
+    }
+
+    @RequestMapping(value = "/stores/store/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable(value = "id") Long id) {
+        storeService.delete(id);
+        return "success";
+    }
+
+}
